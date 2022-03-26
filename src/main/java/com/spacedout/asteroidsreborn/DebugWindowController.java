10445675665dc30abafd35a3132a88d75e776408
Kiran <kiran@inteleyes.com>
2022-03-26 17:44:42 +0000

    Created DebugWindowController.java

---
+++ src/main/java/com/spacedout/asteroidsreborn/DebugWindowController.java
@@ -0,0 +1,9 @@
+package com.spacedout.asteroidsreborn;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+
+public class DebugWindowController {
+
+}
