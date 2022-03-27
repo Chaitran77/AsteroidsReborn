1cf6390bdd707310a4562fb8ee0cd7464001355e
Kiran <kiran@inteleyes.com>
2022-03-27 21:43:31 +0100

    Added startGame method

--- src/main/java/com/spacedout/asteroidsreborn/GameWindowController.java
+++ src/main/java/com/spacedout/asteroidsreborn/GameWindowController.java
@@ -7,5 +7,7 @@ import javafx.scene.control.Label;
 // this is where we can interact with the window (like getting mouse and keyboard events as well as draw to the canvas)
 
 public class GameWindowController {
+	public static void startGame() {
 
+	}
 }
